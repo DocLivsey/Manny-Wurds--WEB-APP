1922187b5ed1ba20c9558e417b05401fc298933c
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-08-28 15:08:03 +0300

    create root app vue file

---
+++ app/src/main/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="header">
+    <header>
+      It`s Main Page
+    </header>
+  </div>
+
+  <div class="main">
+    <h1>
+      Hello Everyone!
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+
+* {
+  margin: 0;
+}
+
+.header {
+  background-color: #2c3e50;
+}
+
+</style>
