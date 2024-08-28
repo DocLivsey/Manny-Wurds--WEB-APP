94f4ce42caef3e445a2e0064878bf582971228dc
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-08-28 23:59:09 +0300

    make dropdown menu as basic component

---
+++ app/src/components/basic/DropdownMenuButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  menu_list: {
+    type: Array,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <div class="dropdown-menu" v-for="menu_item in menu_list">
+    {{ menu_item }}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
