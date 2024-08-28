9ec45cabd7cc83c939d5d9b6372ad9f0d4315753
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-08-29 00:02:29 +0300

    added buttons in profile page

--- app/src/components/profile/buttons/MenuButton.vue
+++ app/src/components/profile/buttons/MenuButton.vue
@@ -1,9 +1,9 @@
 <script setup>
-
+import DropdownMenuButton from '../../basic/DropdownMenuButton.vue'
 </script>
 
 <template>
-
+  <DropdownMenuButton menu_list="Play, Achievement" />
 </template>
 
 <style scoped>
--- app/src/components/profile/buttons/ProfileIcon.vue
+++ app/src/components/profile/buttons/ProfileIcon.vue
@@ -1,9 +1,13 @@
-<script setup>
+<script>
 
 </script>
 
 <template>
-
+  <div class="icon">
+    <button>
+      <img alt="icon" src="../../../assets/public/icon%20_User%20Circle%20icon_.png">
+    </button>
+  </div>
 </template>
 
 <style scoped>
