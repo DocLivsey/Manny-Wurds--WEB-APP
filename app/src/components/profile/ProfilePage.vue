2ad89d4940de417d2adf99cfe6be3d605047b229
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-08-29 00:03:48 +0300

    import buttons in profile page

--- app/src/components/profile/ProfilePage.vue
+++ app/src/components/profile/ProfilePage.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+import ProfileIcon from './buttons/ProfileIcon.vue'
+import MenuButton from "./buttons/MenuButton.vue";
 </script>
 
 <template>
-
+  <MenuButton/>
+  <ProfileIcon/>
 </template>
 
 <style scoped>
