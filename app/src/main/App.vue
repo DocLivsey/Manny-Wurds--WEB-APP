d7cf69918ef5e25515c29fb876fe403a4ede14a9
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-08-29 00:13:56 +0300

    started work on dynamic component viewing

--- app/src/main/App.vue
+++ app/src/main/App.vue
@@ -1,20 +1,39 @@
-<script setup>
+<script>
+import { ref } from 'vue'
 import HomePage from "../components/home/HomePage.vue";
 import ProfilePage from "../components/profile/ProfilePage.vue";
-</script>
+import {defineComponent} from "vue";
+import TabBar from "../components/TabBar.vue";
 
-<script>
-export default {
-  data() {
-    return {
+export default defineComponent({
+  components: {TabBar}
+})
 
-    }
+
+const configuration = {
+  home: {
+    component: HomePage,
+  },
+  profile: {
+    component: ProfilePage,
   }
 }
+
+const selected = ref('text')
 </script>
 
 <template>
-  <HomePage/>
+  <component
+  :is="configuration[selected].component"
+  >
+
+  </component>
+
+  <div class="footer fixed-element">
+    <footer>
+      <TabBar/>
+    </footer>
+  </div>
 </template>
 
 <style scoped>
