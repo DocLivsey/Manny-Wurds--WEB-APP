d33b7ae35eb4c799cbd66d5b5f9fcb10f9ec8bfb
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-09-12 22:51:09 +0300

    starting work to creating menus

--- app/src/components/basic/MenuItem.vue
+++ app/src/components/basic/MenuItem.vue
@@ -1,9 +1,37 @@
 <script setup>
-
+defineProps({
+  menuTitle: {
+    type: String,
+    required: true,
+    default: 'TITLE'
+  },
+  menuDescription: {
+    type: String,
+    required: true,
+    default: 'description',
+  },
+  menuImg: {
+    type: String,
+    required: true,
+    default: '../../assets/public/_TabBar-label.png',
+  }
+})
 </script>
 
 <template>
-
+  <div class="menu-item">
+    <div class="menu-img">
+      <img :src="menuImg" alt="menu-image">
+    </div>
+    <div class="menu-title">
+      <h3>
+        {{ menuTitle }}
+      </h3>
+    </div>
+    <div class="menu-description">
+      {{ menuDescription }}
+    </div>
+  </div>
 </template>
 
 <style scoped>
