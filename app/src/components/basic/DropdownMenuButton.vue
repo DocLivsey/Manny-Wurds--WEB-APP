c5f623594a17f933140bfb166e6347136b155189
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-09-12 21:06:50 +0300

    make dropdown menu a little bit little

--- app/src/components/basic/DropdownMenuButton.vue
+++ app/src/components/basic/DropdownMenuButton.vue
@@ -22,10 +22,12 @@ select {
   background: transparent url("../../assets/public/icon _Bars icon_.png") no-repeat !important;
   border-color: transparent;
   color: transparent;
+  max-height: 32px;
 }
 option {
   background-color: var(--profile-menu-backgroung-color);
   color: #FFFFFF;
   font-family: 'SF Pro Display', sans-serif;
+  font-size: 90%;
 }
 </style>
