e3234d8d51d49a2d09f04ef0e9cb71e88c68a936
DocLivsey <doclivsey@users.noreply.github.com>
2025-01-25 16:38:58 +0300

    style: added same styles as home page

--- app/src/components/game/MainMenuPage.vue
+++ app/src/components/game/MainMenuPage.vue
@@ -3,11 +3,13 @@ import MenuItem from '../basic/MenuItem.vue'
 </script>
 
 <template>
-  <MenuItem
-      menu-title="Timed quiz"
-      menu-description="Guess the word in time"
-      menu-img="app/src/assets/public/timedquiz.png"
-  />
+  <div class="menu-page">
+    <MenuItem
+        menu-title="Timed quiz"
+        menu-description="Guess the word in time"
+        menu-img="app/src/assets/public/timedquiz.png"
+    />
+  </div>
 </template>
 
 <style scoped>
