a0d0290d38c683ac2882e7c0433e919e30888ae6
DocLivsey <doclivsey@users.noreply.github.com>
2025-01-25 23:40:20 +0300

    feat: wrote enum for game modes

--- app/src/main/enums.js
+++ app/src/main/enums.js
@@ -2,3 +2,10 @@ export const ButtonTypes = Object.freeze({
     CHECK: 'Check',
     CLEAR: 'Clear'
 })
+
+export const GameModes = Object.freeze({
+    TIMED_QUIZ: 'TimedQuiz',
+    ATTEMPTED_QUIZ: 'AttemptedQuiz',
+    DAILY_QUIZ: 'DailyQuiz',
+    ONLINE_QUIZ: 'OnlineQuiz'
+})
