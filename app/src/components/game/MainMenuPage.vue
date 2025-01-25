55e18a5ac389ca40661c420d425246e7ecfd4ba4
DocLivsey <doclivsey@users.noreply.github.com>
2025-01-25 16:54:54 +0300

    feat: added new html blocks and styles for its

--- app/src/components/game/MainMenuPage.vue
+++ app/src/components/game/MainMenuPage.vue
@@ -4,14 +4,27 @@ import MenuItem from '../basic/MenuItem.vue'
 
 <template>
   <div class="menu-page">
-    <MenuItem
-        menu-title="Timed quiz"
-        menu-description="Guess the word in time"
-        menu-img="app/src/assets/public/timedquiz.png"
-    />
+    <div class="title-line">
+      Menu
+    </div>
+    <div class="menu-items-list">
+      <MenuItem
+          menu-title="Timed quiz"
+          menu-description="Guess the word in time"
+          menu-img="app/src/assets/public/timedquiz.png"
+      />
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+.menu-items-list {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.title-line {
+  text-transform: uppercase;
+  text-align: center;
+}
 </style>
