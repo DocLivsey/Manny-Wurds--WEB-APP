ebb0d9c63aa25a3f0be25a503f5f6fa625f57066
DocLivsey <doclivsey@users.noreply.github.com>
2025-01-26 00:14:46 +0300

    feat: added new prop for works with game modes

--- app/src/components/basic/MenuItem.vue
+++ app/src/components/basic/MenuItem.vue
@@ -1,4 +1,6 @@
 <script setup>
+import {GameModes} from "@/enums.js";
+
 defineProps({
   menuTitle: {
     type: String,
@@ -14,6 +16,11 @@ defineProps({
     type: String,
     required: true,
     default: '../../assets/public/_TabBar-label.png',
+  },
+  gameMode: {
+    type: GameModes,
+    required: true,
+    default: GameModes.NOT_SELECTED,
   }
 })
 </script>
