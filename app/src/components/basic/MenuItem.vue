8f92e7bedf95c14ee8499c9e01cfc6ea109e9d7a
DocLivsey <doclivsey@github.com>
2025-02-26 14:51:00 +0300

    feat: solve merging conflict

--- app/src/components/basic/MenuItem.vue
+++ app/src/components/basic/MenuItem.vue
@@ -17,16 +17,12 @@ const props = defineProps({
   menuImg: {
     type: String,
     required: true,
-<<<<<<< HEAD
-    default: '../../assets/public/_TabBar-label.png',
+    default: '@/assets/public/_TabBar-label.png',
   },
   gameMode: {
     type: GameModes,
     required: true,
-    default: GameModes.NOT_SELECTED,
-=======
-    default: '@/assets/public/_TabBar-label.png',
->>>>>>> master
+    default: GameModes.NOT_SELECTED,    
   }
 })
 </script>
