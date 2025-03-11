e4724ac1b3efd5b013e096607d70559338a478c1
DocLivsey <doclivsey@github.com>
2025-03-11 12:38:56 +0300

    feat: complete game unit

--- app/src/components/shared/quizPanel/GamePanelUnit.vue
+++ app/src/components/shared/quizPanel/GamePanelUnit.vue
@@ -1,11 +1,36 @@
 <script setup>
-
+defineProps({
+  id: {
+    type: Number,
+    required: true
+  }
+})
 </script>
 
 <template>
-
+  <div class="panel-unit">
+    <input type="text" maxlength="1" :id="`quiz-unit-${id}`">
+  </div>
 </template>
 
 <style scoped>
+.panel-unit {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  height: 3.125rem;
+  width: 3.125rem;
+  background: #FFFFFF;
+  box-shadow: 0 0.5rem 0.5rem 0 rgba(0, 0, 0, 0.25);
+}
 
+.panel-unit input {
+  font: 2rem bolder;
+  width: 100%;
+  height: 100%;
+  background: transparent;
+  border: none;
+  outline: none;
+  text-align: center;
+}
 </style>
