fc8fcea0e51451f744d419316761e5d4d6106dc7
DocLivsey <doclivsey@github.com>
2025-03-11 12:54:04 +0300

    feat: made panel as line block

--- app/src/components/shared/quizPanel/GamePanel.vue
+++ app/src/components/shared/quizPanel/GamePanel.vue
@@ -1,11 +1,42 @@
 <script setup>
+import GamePanelUnit from "./GamePanelUnit.vue";
+import {ref} from "vue";
 
+const unitsList = ref([
+  {
+    id: 1,
+  },
+  {
+    id: 2,
+  },
+  {
+    id: 3,
+  },
+  {
+    id: 4,
+  },
+  {
+    id: 5,
+  },
+  {
+    id: 6,
+  },
+])
 </script>
 
 <template>
-
+  <div class="game-panel">
+    <GamePanelUnit
+        v-for="unit in unitsList"
+        :key="unit.id"
+        :id="unit.id" />
+  </div>
 </template>
 
 <style scoped>
-
+.game-panel {
+  display: flex;
+  flex-direction: row;
+  gap: 0.2rem;
+}
 </style>
